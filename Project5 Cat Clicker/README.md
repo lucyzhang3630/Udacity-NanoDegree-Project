8136417ad2a3e1b1f32d323817eb6d2b4416d23e
lucyzhang3630 <lucyzhang3630@yahoo.com>
2018-05-29 15:52:02 -0500

    add admin status to allow user editing cat information

--- Project5 Cat Clicker/README.md
+++ Project5 Cat Clicker/README.md
@@ -10,6 +10,7 @@
 This is a Sample Cat Clicker Project, written by `html`, `css` and `js`.
 By default, the first cat Amy is selected. You can change the cat by clicking on the cat name in the `nav` bar.
 It increments the count of clicks every time you click on the cat image.
+By clicking on the admin button, will allow you to edit the cat information.
 
 
 To run this project, click on `index.html`, it will run in your default browser
--- Project5 Cat Clicker/js/app.js
+++ Project5 Cat Clicker/js/app.js
@@ -1,26 +1,29 @@
 //model
-var catsModel = [
-  {
-    name:"Amy",
-    counter:0
-  },
-  {
-    name:"Betty",
-    counter:0
-  },
-  {
-    name:"Cathy",
-    counter:0
-  },
-  {
-    name:"Doris",
-    counter:0
-  },
-  {
-    name:"Emily",
-    counter:0
-  }
-];
+var catsModel = {
+  cats:[
+    {
+      name:"Amy",
+      counter:0
+    },
+    {
+      name:"Betty",
+      counter:0
+    },
+    {
+      name:"Cathy",
+      counter:0
+    },
+    {
+      name:"Doris",
+      counter:0
+    },
+    {
+      name:"Emily",
+      counter:0
+    }
+  ],
+  adminStatus:false
+}
 //view
 var navView = {
   init:function() {
@@ -29,7 +32,7 @@ var navView = {
   },
   render:function() {
     var catsList = $('ul.catsList');
-    catsModel.forEach(function(cat,index) {
+    catsModel.cats.forEach(function(cat,index) {
       catsList.append('<li class="catItem"'+' id='+index+'>'+cat.name+'</li>')
     });
     octopus.selectCat();
@@ -37,6 +40,7 @@ var navView = {
 }
 var catView = {
   init:function() {
+    $('div.catInfo').hide();
     octopus.updateCatView(0);
     catView.render();
   },
@@ -45,7 +49,10 @@ var catView = {
       var index = e.target.id;
       octopus.updateCatView(index)
     });
-    octopus.handleClick()
+    octopus.handleClick();
+    octopus.toggleEdit();
+    octopus.saveEdit();
+    octopus.cancelEdit();
   }
 }
 //octopus
@@ -55,22 +62,57 @@ var octopus = {
     $('li.catItem').on('click',function(e) {
       var activeCat= $('li.active').removeClass('active');
       $(e.target).addClass('active');
+      //when select another cat, change admin status to default value;
+      catsModel.adminStatus = false;
     })
   },
   updateCatView:function(catIndex){
-    var cat = catsModel[catIndex];
+    var cat = catsModel.cats[catIndex];
     var imageSrc = 'images/'+cat.name +'.jpg';
     $('img.catImage').attr('src',imageSrc);
     $('img.catImage').attr('id',catIndex);
     $('h1.catName').html(cat.name);
     $('span.clicks').html(cat.counter);
+    $('div.catInfo').hide();
   },
   handleClick: function() {
     //increase the counter and update the view for counter
     $('img.catImage').on('click',function(e) {
       var index = e.target.id;
-      catsModel[index].counter++;
-      $('span.clicks').html(catsModel[index].counter);
+      catsModel.cats[index].counter++;
+      $('span.clicks').html(catsModel.cats[index].counter);
+    })
+  },
+  toggleEdit:function() {
+    $('button.adminBtn').on('click',function() {
+      catsModel.adminStatus =! catsModel.adminStatus;
+      if(catsModel.adminStatus) {
+        var index;
+        $('div.catInfo').show();
+        index = $('img.catImage').attr('id');
+        $('input.nameInput').val(catsModel.cats[index].name);
+        $('input.clicksInput').val(catsModel.cats[index].counter);
+      }else{
+        $('div.catInfo').hide();
+      }
+    })
+  },
+  saveEdit:function() {
+    $('button.saveBtn').on('click',function() {
+      var index = $('img.catImage').attr('id');
+      catsModel.cats[index].name = $('input.nameInput').val();
+      catsModel.cats[index].counter = $('input.clicksInput').val();
+      catsModel.adminStatus = false;
+      $('div.catInfo').hide();
+      $('li#'+index).html(catsModel.cats[index].name);
+      $('h1.catName').html(catsModel.cats[index].name);
+      $('span.clicks').html(catsModel.cats[index].counter);
+    })
+  },
+  cancelEdit:function(){
+    $('button.cancelBtn').on('click',function() {
+      catsModel.adminStatus = false;
+      $('div.catInfo').hide();
     })
   },
   init:function() {
