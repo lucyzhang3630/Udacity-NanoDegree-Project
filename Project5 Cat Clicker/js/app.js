114cd98f06b5da33747df85f563431080efec81b
lucyzhang3630 <lucyzhang3630@yahoo.com>
2018-05-28 19:35:18 -0500

    refactor the code following MV* pattern

--- Project5 Cat Clicker/js/app.js
+++ Project5 Cat Clicker/js/app.js
@@ -1,18 +1,82 @@
-var counter = 0;
-function handleClick() {
-  var catImage = $('img.catImage');
-  var clicks = $('span.clicks');
-  counter++;
-  clicks.html(counter);
-};
-function selectCat(e) {
-  //change the active status
-  var activeCat= $('li.active').removeClass('active');
-  $(e.target).addClass('active');
-  //update image and cat name
-  var imageSrc = 'images/'+e.target.textContent +'.jpg';
-  $('img.catImage').attr('src',imageSrc);
-  $('h1.catName').html(e.target.textContent);
-  counter = 0;
-  $('span.clicks').html(counter);
+//model
+var catsModel = [
+  {
+    name:"Amy",
+    counter:0
+  },
+  {
+    name:"Betty",
+    counter:0
+  },
+  {
+    name:"Cathy",
+    counter:0
+  },
+  {
+    name:"Doris",
+    counter:0
+  },
+  {
+    name:"Emily",
+    counter:0
+  }
+];
+//view
+var navView = {
+  init:function() {
+    navView.render();
+    $('li.catItem:first-child').addClass('active');
+  },
+  render:function() {
+    var catsList = $('ul.catsList');
+    catsModel.forEach(function(cat,index) {
+      catsList.append('<li class="catItem"'+' id='+index+'>'+cat.name+'</li>')
+    });
+    octopus.selectCat();
+  }
 }
+var catView = {
+  init:function() {
+    octopus.updateCatView(0);
+    catView.render();
+  },
+  render: function() {
+    $('li.catItem').on('click',function(e) {
+      var index = e.target.id;
+      octopus.updateCatView(index)
+    });
+    octopus.handleClick()
+  }
+}
+//octopus
+var octopus = {
+  selectCat:function() {
+    //change active status
+    $('li.catItem').on('click',function(e) {
+      var activeCat= $('li.active').removeClass('active');
+      $(e.target).addClass('active');
+    })
+  },
+  updateCatView:function(catIndex){
+    var cat = catsModel[catIndex];
+    var imageSrc = 'images/'+cat.name +'.jpg';
+    $('img.catImage').attr('src',imageSrc);
+    $('img.catImage').attr('id',catIndex);
+    $('h1.catName').html(cat.name);
+    $('span.clicks').html(cat.counter);
+  },
+  handleClick: function() {
+    //increase the counter and update the view for counter
+    $('img.catImage').on('click',function(e) {
+      var index = e.target.id;
+      catsModel[index].counter++;
+      $('span.clicks').html(catsModel[index].counter);
+    })
+  },
+  init:function() {
+    navView.init();
+    catView.init();
+  }
+}
+
+octopus.init();
