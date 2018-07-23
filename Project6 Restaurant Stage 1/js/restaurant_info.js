ed36703ac7680d4deba5390a7ba3665a95944749
lucyzhang3630 <lucyzhang3630@yahoo.com>
2018-07-23 17:54:34 -0500

    css for detail page for Galaxy S5

--- Project6 Restaurant Stage 1/js/restaurant_info.js
+++ Project6 Restaurant Stage 1/js/restaurant_info.js
@@ -149,13 +149,16 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
 createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
+  const div = document.createElement('div');
   name.innerHTML = review.name;
-  li.appendChild(name);
+  div.appendChild(name);
+  //li.appendChild(name);
 
   const date = document.createElement('p');
   date.innerHTML = review.date;
-  li.appendChild(date);
-
+  div.appendChild(date);
+  //li.appendChild(date);
+  li.appendChild(div);
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
   li.appendChild(rating);
